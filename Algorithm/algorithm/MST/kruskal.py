766edfe9a42faf0efc8150ee336c9df492a08e62
breathinlee <breathinlee@gmail.com>
2021-10-28 23:06:48 +0900

    algo

---
+++ Algorithm/algorithm/MST/kruskal.py
@@ -0,0 +1,39 @@
+def make_set(x):
+    p[x] = x                    
+
+def find_set(x):
+    if p[x] != x:                
+        p[x] = find_set(p[x])  
+    return p[x]                 
+
+def union(x, y):
+    p[find_set(y)] = find_set(x) 
+
+
+def kruskal():
+    global ans
+    edge_cnt = idx = 0                       
+
+    while edge_cnt != V:                    
+        x = edges[idx][0]                    
+        y = edges[idx][1]
+                                            
+        if find_set(x) != find_set(y):       
+            union(x, y)                      
+            edge_cnt += 1                   
+            ans += edges[idx][2]             
+        idx += 1                             
+                                            
+
+import sys
+sys.stdin = open('input.txt')
+ans = 0
+V, E = map(int, input().split())
+edges = [list(map(int, input().split())) for _ in range(E)]  
+p = [0] * (V+1)                                             
+edges = sorted(edges, key=lambda x: x[2])                   
+
+for i in range(V+1):
+    make_set(i)
+kruskal()
+print(ans)                                                   
