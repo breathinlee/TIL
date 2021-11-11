af57105fea81cd29f13dce704665296af99c5727
breathinlee <breathinlee@gmail.com>
2021-11-11 23:52:41 +0900

    make todo list by vuex

---
+++ vue.js/vuex-todo-list/src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <todo-list-item
+      v-for="(todo, index) in todos" 
+      :key="index"
+      :todo="todo"
+    ></todo-list-item>
+  </div>
+</template>
+
+<script>
+import TodoListItem from '@/components/TodoListItem'
+export default {
+  name: 'TodoList',
+  components: {
+    TodoListItem,
+  },
+  computed: {
+    todos: function() {
+      return this.$store.state.todos
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
