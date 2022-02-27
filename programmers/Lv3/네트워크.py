39874e0bb622c2cfa0e491e030bee2f663e0a5d2
breathinlee <breathinlee@gmail.com>
2022-02-27 23:23:55 +0900

    add programmers lv3 네트워크 bfs sol

--- programmers/Lv3/네트워크.py
+++ programmers/Lv3/네트워크.py
@@ -1,3 +1,5 @@
+# dfs
+
 def dfs(start, computers, visited): 
     visited[start] = 1
     
@@ -14,4 +16,28 @@ def solution(n, computers):
         if not visited[r]:
             dfs(r, computers, visited)
             answer += 1
+    return answer
+
+
+# bfs
+
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
+    visited = [0] * n
+    queue = deque()
+    
+    for start in range(n):
+        if not visited[start]:
+            queue.append(start)
+            while queue:
+                k = queue.pop()
+                visited[k] = 1
+                
+                for s in range(n):
+                    if computers[k][s] and not visited[s]:
+                        queue.append(s)
+            answer += 1
+        
     return answer
