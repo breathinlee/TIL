d5838d08d0cafc1ae6fc04b92f83e4f32c5efb31
breathinlee <breathinlee@gmail.com>
2022-03-01 12:38:30 +0900

    add programmers lv3 sol

--- programmers/Lv3/단어변환.py
+++ programmers/Lv3/단어변환.py
@@ -1,3 +1,36 @@
+# sol
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+
+    answer = 0
+    stack = [begin]
+
+    while len(words):
+        for s in stack:
+            next = []
+            for word in words:
+                cnt = 0
+                for k in range(len(begin)):
+                    if word[k] != s[k]:
+                        cnt += 1
+                    if cnt >= 2:
+                        break
+                if cnt == 1:  # begin이 words안에 존재할 수 있음
+                    next.append(word)
+                    words.remove(word)
+        answer += 1
+
+        if target in next:
+            return answer
+        stack = next
+
+    return answer
+
+
+
+
+
 # ver1 (미완성)
 
 from collections import deque
@@ -29,3 +62,4 @@ def solution(begin, target, words):
         
     
     return answer
+
