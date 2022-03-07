e2636e69b1e5fac71615cacae8de7425692a3579
breathinlee <breathinlee@gmail.com>
2022-03-07 23:02:43 +0900

    update boj silver1 problem sol

--- boj/1743_음식물 피하기.py
+++ boj/1743_음식물 피하기.py
@@ -1,3 +1,5 @@
+# sol1
+
 import sys
 sys.setrecursionlimit(10**6)
 
@@ -31,9 +33,48 @@ max_cnt = 0
 
 for r in range(N):
     for c in range(M):
-        if garbage[r][c] == 1 and not visited[r][c]:
+        if garbage[r][c] and not visited[r][c]:
             cnt = 0
             visited[r][c] = 1
             max_cnt = max(max_cnt, dfs(r, c))
+
+print(max_cnt)
+
+
+# sol2
+
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(r, c, cnt):
+    visited[r][c] = 1
+
+    for k in range(4):
+        nr = r + dr[k]
+        nc = c + dc[k]
+
+        if 0 <= nr < N and 0 <= nc < M and garbage[nr][nc] and not visited[nr][nc]:
+            cnt = dfs(nr, nc, cnt + 1)
+
+    return cnt
+
+
+input = sys.stdin.readline
+N, M, K = map(int, input().split()) 
+garbage = [[0] * M for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+for _ in range(K):
+    a, b = map(int, input().split())
+    garbage[a - 1][b - 1] = 1
+
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
+max_cnt = 0
+
+for r in range(N):
+    for c in range(M):
+        if garbage[r][c] and not visited[r][c]:
+            ret = dfs(r, c, 1)
+            max_cnt = max(max_cnt, ret)
 
 print(max_cnt)
