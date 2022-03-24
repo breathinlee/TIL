8b6fae3025ecae0540dcc8650b1d86311547503c
breathinlee <breathinlee@gmail.com>
2022-03-24 23:47:39 +0900

    add boj 1309 sol

--- boj/1309_동물원.py
+++ boj/1309_동물원.py
@@ -7,3 +7,18 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+
+dp = [[0] * 3 for _ in range(N+1)]
+
+dp[1][0] = dp[1][1] = dp[1][2] = 1
+
+for i in range(2, N+1):
+    dp[i][0] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
+    dp[i][0] %= 9901
+    dp[i][1] = dp[i-1][0] + dp[i-1][2]
+    dp[i][1] %= 9901
+    dp[i][2] = dp[i-1][0] + dp[i-1][1]
+    dp[i][2] %= 9901
+
+answer = sum(dp[N]) % 9901
+print(answer)
