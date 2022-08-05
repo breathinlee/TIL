cd53d2a6431fc47370f04721d5a8edba380f8a14
breathinlee <breathinlee@gmail.com>
2022-08-05 19:22:46 +0900

    add boj 18428 another sol

--- boj/18428_감시 피하기.py
+++ boj/18428_감시 피하기.py
@@ -1,3 +1,5 @@
+# sol 1 (88ms)
+
 from itertools import combinations
 import sys
 input = sys.stdin.readline
@@ -52,3 +54,65 @@ if flag:
     print('YES')
 else:
     print('NO')
+
+
+# sol 2 (160ms)
+
+import sys
+input = sys.stdin.readline
+
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
+
+def search():
+    for teacher in teachers:
+        for k in range(4):
+            r, c = teacher[0], teacher[1]
+
+            while 0 <= r < N and 0 <= c < N:
+                if arr[r][c] == 'O':
+                    break
+                elif arr[r][c] == 'S':
+                    return False
+
+                r += dr[k]
+                c += dc[k]
+
+    return True
+
+
+def dfs(cnt):
+    global flag
+
+    if cnt == 3:
+        if search():
+            flag = True
+
+        return
+
+    for r in range(N):
+        for c in range(N):
+            if arr[r][c] == 'X':
+                arr[r][c] = 'O'
+                cnt += 1
+                dfs(cnt)
+                arr[r][c] = 'X'
+                cnt -= 1
+
+
+N = int(input())
+arr = []
+teachers = []
+
+flag = False
+
+for r in range(N):
+    arr.append(list(input().split()))
+
+    for c in range(N):
+        if arr[r][c] == 'T':
+            teachers.append((r, c))
+
+dfs(0)
+
+print('YES' if flag else 'NO')
