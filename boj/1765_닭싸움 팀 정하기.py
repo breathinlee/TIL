cbd7e2d087ea326087bb9cad056653febc643fd0
breathinlee <breathinlee@gmail.com>
2022-09-06 21:24:41 +0900

    RENAME variable friend to parent

--- boj/1765_닭싸움 팀 정하기.py
+++ boj/1765_닭싸움 팀 정하기.py
@@ -2,19 +2,19 @@ import sys
 input = sys.stdin.readline
 
 def find(x):
-    if friend[x] != x:
-        friend[x] = find(friend[x])
-    return friend[x]
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
 
 
 def union(a, b):
     a, b = find(a), find(b)
-    friend[b] = a
+    parent[b] = a
 
 
 n = int(input())
 m = int(input())
-friend = [i for i in range(n+1)]
+parent = [i for i in range(n+1)]
 enemy = [[] for _ in range(n+1)]
 for _ in range(m):
     r, a, b = input().split()
@@ -38,4 +38,4 @@ for _ in range(m):
 for i in range(1, n+1):
     find(i)
 
-print(len(set(friend))-1)
+print(len(set(parent))-1)
