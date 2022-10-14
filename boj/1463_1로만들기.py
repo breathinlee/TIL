24457e15da74c88b1af0d1e3b1df57d593759e31
breathinlee <breathinlee@gmail.com>
2022-10-14 17:29:50 +0900

    Add boj 1463 sol

---
+++ boj/1463_1로만들기.py
@@ -0,0 +1,13 @@
+N = int(input())
+dp = [0] * (N+1)
+
+for k in range(2, N+1):
+    dp[k] = dp[k - 1] + 1
+
+    if k % 2 == 0:
+        dp[k] = min(dp[k], dp[k // 2] + 1)
+
+    if k % 3 == 0:
+        dp[k] = min(dp[k], dp[k // 3] + 1)
+
+print(dp[N])
