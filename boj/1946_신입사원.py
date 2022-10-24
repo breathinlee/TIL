0a0b285c6176a336bbfb59cae35b48932424788f
breathinlee <breathinlee@gmail.com>
2022-10-24 23:57:27 +0900

    Add boj 1946 another sol

--- boj/1946_신입사원.py
+++ boj/1946_신입사원.py
@@ -1,3 +1,5 @@
+# sol 1 (4876ms)
+
 import sys
 input = sys.stdin.readline
 
@@ -14,5 +16,30 @@ for tc in range(T):
         if sorted_members_score[k][1] < standard:
             result += 1
             standard = sorted_members_score[k][1]
+
+    print(result)
+
+
+
+# sol 2 (2452ms + 메모리도 절반 이하)
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    members_rank = [0] * (N+1)
+    for _ in range(N):
+        first, second = map(int, input().rstrip().split())
+        members_rank[first] = second
+
+    result = 0
+    standard = N+1
+
+    for k in range(1, N+1):
+        if members_rank[k] < standard:
+            result += 1
+            standard = members_rank[k]
 
     print(result)
