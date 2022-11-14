3e20cd32d4574ccc9eb1f7991a0240defa4ae779
breathinlee <breathinlee@gmail.com>
2022-11-14 21:32:36 +0900

    Add boj 1759 sol

--- boj/1759_암호만들기.py
+++ boj/1759_암호만들기.py
@@ -1,1 +1,32 @@
-# 1759_암호 만들기
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+vowels = list('aeiou')
+consonants = list('bcdfghjklmnpqrstvwxyz')
+
+L, C = map(int, input().rstrip().split())
+characters = list(input().split())
+cases = combinations(characters, L)
+answer = []
+
+for case in cases:
+    cnt = 0
+
+    for vowel in vowels:
+        if vowel in case:
+            cnt += 1
+
+    if cnt < 1:
+        continue
+
+    elif cnt > L - 2:
+        continue
+
+    else:
+        sort_case = sorted(case)
+        answer.append(''.join(sort_case))
+
+answer.sort()
+for ans in answer:
+    print(ans)
